476a1f438d2a3feab572a535135952c17eb204e7
antonio123-eng <80233551+antonio123-eng@users.noreply.github.com>
2022-08-20 10:09:00 -0300

    react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './components/home/Home';
+
+function App() {
+  return (
+    <div>
+      <h1>Teste</h1>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/Home.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+
+const Home = () => {
+
+  const [photos, setPhotos] = useState([])
+
+  useEffect(() => {
+    
+    const fetchPhoto = async() => {
+
+      const res = await fetch("https://teste-deploy-toughts.herokuapp.com/api/photos")
+      const data = await res.json()
+      console.log(data)
+
+      setPhotos(data)
+      console.log(photos)
+    }
+    fetchPhoto()
+  }, [])
+
+  return (
+    <div>
+      {photos && photos.map((photo) => (
+        <div>
+          <h3>{photo.title}</h3>
+          <img src={photo.image} alt={photo.title} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Home
