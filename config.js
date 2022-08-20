d0727bbcfb421e17f54482269c846b0a7d9c00e1
antonio123-eng <80233551+antonio123-eng@users.noreply.github.com>
2022-08-20 11:36:34 -0300

    fd

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+   async headers() {
+      return {
+         source: "/:path*",
+         headers: [
+            {key: "Access-Control-Allow-Credentials", value: "true"},
+            {key: "Access-Control-Allow-Origin", value: "*"},
+            {key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, POST, DELETE, PUT"},
+            {key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Authorization"}
+         ]
+      }
+   }
+}
