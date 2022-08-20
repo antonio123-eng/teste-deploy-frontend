f9af16b0e180e814fba79f6d5a3e5dfc66c5942d
antonio123-eng <80233551+antonio123-eng@users.noreply.github.com>
2022-08-20 10:39:59 -0300

    bnmnmwe

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
     
     const fetchPhoto = async() => {
 
-      const res = await fetch("https://teste-deploy-toughts.herokuapp.com/api/photos")
+      const res = await fetch("https://teste-deploy-toughts.herokuapp.com/", {mode: 'cors'})
       const data = await res.json()
       console.log(data)
 
