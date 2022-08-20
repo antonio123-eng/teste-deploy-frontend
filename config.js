82b37e527a9e759882a83574d3cfe9542ec7b2cc
antonio123-eng <80233551+antonio123-eng@users.noreply.github.com>
2022-08-20 11:39:56 -0300

    rrettyty

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
             {key: "Access-Control-Allow-Credentials", value: "true"},
             {key: "Access-Control-Allow-Origin", value: "*"},
             {key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, POST, DELETE, PUT"},
-            {key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Authorization"}
+            {key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With"}
          ]
       }
    }
